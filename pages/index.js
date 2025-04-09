8c2a9b82fef1a992e1d7564083890464a0f0e46d
cwright3bsu <cwright3@bsu.edu>
2025-04-09 08:41:28 -0400

    Add files via upload

--- pages/index.js
+++ pages/index.js
@@ -1,11 +1,12 @@
+
 import React from 'react';
 import Interview from '../components/Interview';
 
 export default function Home() {
   return (
-    <div>
-      <h1>CBP GPT Interview Simulator</h1>
+    <main style={{ padding: 20 }}>
+      <h1>CBP-GPT Interview Simulation</h1>
       <Interview />
-    </div>
+    </main>
   );
 }
